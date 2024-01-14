8c352e7bd95b1d0297b244c31c8af47f3272141a
jscanzi <jscanzi@cisco.com>
2024-01-14 10:39:26 +0100

    Finish basic RPC behavior; Implement RL; Beautify code

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,42 @@
-use hyper::service::{service_fn};
-use hyper::{Request, Response};
-use std::collections::HashMap;
-use tokio::net::TcpListener;
-use hyper_util::rt::TokioIo;
+use http_body_util::{BodyExt, Full};
 use hyper::server::conn::http1;
-use http_body_util::{Full, BodyExt};
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use std::collections::HashMap;
 use std::net::SocketAddr;
+use tokio::net::TcpListener;
 
+use hyper::body::Bytes;
 use std::convert::Infallible;
-use hyper::body::{Bytes, Buf};
 
-use std::sync::Arc;
 use futures::lock::Mutex;
-use futures::lock::MutexLockFuture;
+use std::sync::Arc;
 
-use std::io::Read;
-//use futures::executor::block_on;
+use tokio::time::Duration;
+use arl::RateLimiter;
+
+use regex::Regex;
 
 #[derive(Hash, PartialEq, Eq, Clone)]
 struct UserKey {
     key: String,
 }
 
+impl UserKey {
+    fn new(key: String) -> Self {
+        Self { key: key }
+    }
+}
+
 struct LocalDbHandler {
     db: HashMap<UserKey, String>,
+}
+
+enum SetResult {
+    SucessNewEntry,
+    SucessEntryExisted,
+    // Failure,
 }
 
 impl LocalDbHandler {
@@ -51,42 +64,63 @@ impl LocalDbHandler {
             println!("Key: {}, value: {}", k.key, v);
         }
     }
+    async fn get(&self, user: &str) -> Option<String> {
+        self.db.get(&UserKey::new(user.to_string())).cloned()
+    }
+
+    async fn set(&mut self, user: &str, value: &str) -> SetResult {
+        match self
+            .db
+            .insert(UserKey::new(user.to_string()), value.to_string())
+        {
+            Some(_) => SetResult::SucessEntryExisted,
+            None => SetResult::SucessNewEntry,
+        }
+    }
 }
 
-
+const RL_TIMESLOT_SIZE_SECONDS: u64 = 10;
+const RL_MAX_QUERIES_PER_TIMESLOT: usize = 5;
 
 #[tokio::main]
 async fn main() {
-    let mut dbh = Arc::new(Mutex::new(LocalDbHandler::new()));
+    let dbh = Arc::new(Mutex::new(LocalDbHandler::new()));
+    // Async stuff experiments
     async {
-        dbh.clone().lock().await.update_elem(&"bob".to_string(), "dhbfjnbgjh".to_string())
+        dbh.clone()
+            .lock()
+            .await
+            .update_elem(&"bob".to_string(), "dhbfjnbgjh".to_string())
             .await;
         dbh.clone().lock().await.dump_terminal().await;
-        dbh.clone().lock().await.apply_to_all(|x| x.to_uppercase()).await;
+        dbh.clone()
+            .lock()
+            .await
+            .apply_to_all(|x| x.to_uppercase())
+            .await;
         dbh.clone().lock().await.dump_terminal().await;
     }
     .await;
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
     let listener = TcpListener::bind(&addr).await.unwrap();
     println!("Server listening on http://{}", addr);
-
+    let limiter = RateLimiter::new(RL_MAX_QUERIES_PER_TIMESLOT, Duration::from_secs(RL_TIMESLOT_SIZE_SECONDS));
 
     // We start a loop to continuously accept incoming connections
     loop {
         let (stream, _) = listener.accept().await.unwrap();
 
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
         let io = TokioIo::new(stream);
         let dbhh = dbh.clone();
+        let limiter = limiter.clone();
+        
         // Spawn a tokio task to serve multiple connections concurrently
         tokio::task::spawn(async move {
-            // Finally, we bind the incoming connection to our `hello` service
+            limiter.wait().await;
             if let Err(err) = http1::Builder::new()
                 // `service_fn` converts our function in a `Service`
-            .serve_connection(io, service_fn(|req| hello(req, dbhh.clone())))
+                .serve_connection(io, service_fn(|req| hello(req, dbhh.clone())))
                 .await
             {
                 println!("Error serving connection: {:?}", err);
@@ -95,7 +129,10 @@ async fn main() {
     }
 }
 
-async fn hello(r: Request<hyper::body::Incoming>, dbh: Arc<Mutex<LocalDbHandler>>) -> Result<Response<Full<Bytes>>, Infallible> {
+async fn hello(
+    r: Request<hyper::body::Incoming>,
+    dbh: Arc<Mutex<LocalDbHandler>>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
     //let mut buf = vec![];
     //let mut test = r.into_body().collect().await.unwrap().aggregate().reader();
     let test = r.into_body().collect().await.unwrap().to_bytes();
@@ -103,13 +140,64 @@ async fn hello(r: Request<hyper::body::Incoming>, dbh: Arc<Mutex<LocalDbHandler>
     match test {
         Ok(text) => {
             println!("body: {}", text);
-           // let re = Regex::new(r"(a-z)+;(a-zA-Z0-9)+$").unwrap();
-           // if (text.contains("GET"))
-         //   }
-    },
+            let re = Regex::new(r"([a-z]+);+([a-zA-Z0-9]+);*(.*)$").unwrap();
+            let groups = re.captures(text);
+            if let Some(groups) = groups {
+                let command = groups.get(1);
+                let user = groups.get(2);
+                if let Some(command) = command {
+                    if let Some(user) = user {
+                        match command.as_str() {
+                            "get" => {
+                                let res = dbh.lock().await.get(user.as_str()).await;
+                                match res {
+                                    Some(s) => {
+                                        let returned_text = format!("Success, value is {}\n", s);
+                                        return Ok(Response::new(Full::new(Bytes::from(
+                                            returned_text,
+                                        ))));
+                                    }
+                                    None => {
+                                        return Ok(Response::new(Full::new(Bytes::from(
+                                            "No user could be found\n",
+                                        ))))
+                                    }
+                                }
+                            }
+                            "set" => {
+                                if let Some(value) = groups.get(3) {
+                                    let res =
+                                        dbh.lock().await.set(user.as_str(), value.as_str()).await;
+                                    match res {
+                                        SetResult::SucessNewEntry => return Ok(Response::new(Full::new(Bytes::from(
+                                            "Entry added\n",
+                                        )))),
+                                        SetResult::SucessEntryExisted => return Ok(Response::new(Full::new(Bytes::from(
+                                            "Entry added, user already existed, value was replaced\n",
+                                        )))),
+                                    }
+                                } else {
+                                    return Ok(Response::new(Full::new(Bytes::from(
+                                        "Invalid set value\n",
+                                    ))));
+                                }
+                            }
+                            _ => {
+                                return Ok(Response::new(Full::new(Bytes::from(
+                                    "Invalid command\n",
+                                ))));
+                            }
+                        }
+                    } else {
+                        return Ok(Response::new(Full::new(Bytes::from("Invalid user\n"))));
+                    }
+                } else {
+                    return Ok(Response::new(Full::new(Bytes::from("Invalid command\n"))));
+                }
+            }
+        }
         Err(_) => println!("Failed to get body"),
     }
 
-
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+    Ok(Response::new(Full::new(Bytes::from("Could not process request\n"))))
 }
